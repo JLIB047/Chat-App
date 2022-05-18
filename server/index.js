fd3d32bfab4c8d40930b6786cdc2155771a9c184
Jason Libertelli <jason.libertelli@gmail.com>
2022-05-18 17:32:40 -0400

    twilio push notifications

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 
 //allow to call environment variables 
 require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken);
 
 app.use(cors()); 
 //pass json from frontend to backend 
@@ -18,6 +22,28 @@ app.use(express.urlencoded());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.post('/', (req, res) => {
+    const {message, user: sender, type, members } = req.body;
+
+    if(type === "message.new") {
+        members.
+            filter((member) => member.user_id !== sender.id)
+            forEach(({ user }) => {
+            if(!user.online) {
+                twilioClient.messages.create({
+                    bode: `You have a new Message from ${message.user.fullName} - ${message.text}`,
+                    messagingServiceSid: messagingServiceSid,
+                    to: user.phoneNumber
+                })
+                    .then(() => console.log('Message Sent!'))
+                    .catch((err) => console.log(err))
+            }
+        })
+
+        return res.status(200).send('Message Sent!')
+    }
+    return res.status(200).send('Not a new message request')
+})
 
 app.use('/auth', authRoutes);
 
